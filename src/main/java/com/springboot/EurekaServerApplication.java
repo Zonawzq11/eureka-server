53bbcc750c4ca0500192549aecf57b10e30d5d11
王志强 <wangzq@cjqc.com>
2018-11-28 15:07:41 +0800

    Initial commit

---
+++ src/main/java/com/springboot/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/***
+ * 用@EnableEurekaServer 注解启动一个服务中心提供其他应用进行对话
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
